b2255889ca8f549ef123bd4ca63016c8dd364c74
Austin White <austinlldb@gmail.com>
2023-03-18 12:35:18 -0500

    static site generator

--- src/format-index.js
+++ src/format-index.js
@@ -4,29 +4,6 @@ import { getStylesheetTag, getPostMetadata } from "./util.js";
 import { topBar } from "./top-bar.js";
 import * as fs from "fs";
 
-function getFront(stream, stylesheets) {
-  stream.push("<!DOCTYPE html>\n");
-  stream.push(
-    '<meta content="width=device-width, initial-scale=1.0" name="viewport">\n'
-  );
-  stream.push('<html lang="en">\n');
-  stream.push("<head>\n");
-  stream.push(
-    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n'
-  );
-  stream.push(
-    '<meta name="description" content="The personal website of Austin White">\n'
-  );
-  stream.push("<title>Austin White</title>\n");
-  stream.push('<link rel="shortcut icon" href="../site/rsc/favicon.ico">\n');
-  stream.push(
-    stylesheets
-      .map((stylesheet) => getStylesheetTag("../site/rsc", stylesheet))
-      .join("\n") + "\n"
-  );
-  stream.push("</head>\n");
-}
-
 function getHeroImagePath(postPath) {
   const postImagesPath = "posts/images/" + postPath.split(/[/.]+/)[1];
   const heroImageName = fs
@@ -57,13 +34,49 @@ function convertDate(date) {
   return months[parseInt(date[0])] + " " + date[1] + ", " + date[2];
 }
 
+function sortPosts(postPaths) {
+  postPaths.sort((a, b) => {
+    let { date: aDate } = getPostMetadata(a);
+    let { date: bDate } = getPostMetadata(b);
+    aDate = Math.floor(new Date(aDate).getTime() / 1000);
+    bDate = Math.floor(new Date(bDate).getTime() / 1000);
+
+    return bDate > aDate ? 1 : -1;
+  });
+}
+
+function getFront(stream, stylesheets) {
+  stream.push("<!DOCTYPE html>\n");
+  stream.push(
+    '<meta content="width=device-width, initial-scale=1.0" name="viewport">\n'
+  );
+  stream.push('<html lang="en">\n');
+  stream.push("<head>\n");
+  stream.push(
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n'
+  );
+  stream.push(
+    '<meta name="description" content="The personal website of Austin White">\n'
+  );
+  stream.push("<title>Austin White</title>\n");
+  stream.push('<link rel="shortcut icon" href="../site/rsc/favicon.ico">\n');
+  stream.push(
+    stylesheets
+      .map((stylesheet) => getStylesheetTag("../site/rsc", stylesheet))
+      .join("\n") + "\n"
+  );
+  stream.push("</head>\n");
+}
+
 function generateBody(stream, postPaths) {
+  sortPosts(postPaths);
+
   stream.push("<body>\n");
   stream.push("<div class='container'>\n");
 
-  stream.push("<header>\n")
+  stream.push("<header>\n");
   topBar(stream);
-  stream.push("</header>\n")
+  stream.push("</header>\n");
 
   postPaths.map((postPath) => {
     const { title, date, summary } = getPostMetadata(postPath);
@@ -73,7 +86,11 @@ function generateBody(stream, postPaths) {
     stream.push("<div class='post'>\n");
     stream.push("<div class='hero'>\n");
     stream.push('<a href="' + htmlPath + '">\n');
-    stream.push('<img src="' + heroImagePath + '" alt="' + title + '">\n');
+    stream.push("<div class='img-container'>");
+    stream.push(
+      '<img src="' + heroImagePath + '" class="clip" alt="' + title + '">\n'
+    );
+    stream.push("</div>\n");
     stream.push("</a>\n");
     stream.push("</div>\n");
     stream.push("<div class='postInfo'>\n");
